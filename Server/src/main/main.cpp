3d1c4ded364329614e04331a3178d5ceac6dbb61
RomanKhrapchun <khrapchun@gmail.com>
2024-11-24 12:36:01 +0200

    to pizda ya yebenu

--- Server/src/main/main.cpp
+++ Server/src/main/main.cpp
@@ -2,7 +2,9 @@
 #include "../../include/service/SystemResourceService.hpp"
 #include "../../include/util/ArgvUtil.hpp"
 
-
+#include "Resource.сpp"
+#include "../service/SystemResourceService.сpp"
+#include "../util/ArgvUtil.сзp"
 
 #include <csignal>
 
